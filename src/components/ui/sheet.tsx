c8095db735f61c16163781a99564c898e5355651
Yasu Fadhil <yasufad@proton.me>
2026-08-14 10:40:37 +0300

    fix: use Phosphor sheet close icon

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -2,10 +2,10 @@
 
 import * as React from "react"
 import { Dialog as SheetPrimitive } from "@base-ui/react/dialog"
+import { XIcon } from "@phosphor-icons/react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
-import { XIcon } from "lucide-react"
 
 function Sheet({ ...props }: SheetPrimitive.Root.Props) {
   return <SheetPrimitive.Root data-slot="sheet" {...props} />
