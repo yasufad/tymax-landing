18e26136b0280518836c8a877f1e2bb863137ac9
Yasu Fadhil <yasufad@proton.me>
2026-08-14 15:16:46 +0300

    fix: use Phosphor dialog close icon

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -2,10 +2,10 @@
 
 import * as React from "react"
 import { Dialog as DialogPrimitive } from "@base-ui/react/dialog"
+import { XIcon } from "@phosphor-icons/react"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
-import { XIcon } from "lucide-react"
 
 function Dialog({ ...props }: DialogPrimitive.Root.Props) {
   return <DialogPrimitive.Root data-slot="dialog" {...props} />
